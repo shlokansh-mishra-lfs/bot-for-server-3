cb67531d21562b17a617a47a52d05dfc9a82d41f
Shlokansh mishra <risingstargames.email@gmail.com>
2025-11-26 10:49:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8003
 
 app.get('/', (req, res) => {
   res.send('Bot has arrived');
